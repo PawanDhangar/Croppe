5e21257ec6f4ad8c3e2caa6d2e962b521d7af7c5
Pawan <pawandhangar113@gmail.com>
2023-03-19 10:10:49 +0530

    Resolved all conflicts

--- src/Components/index.jsx
+++
@@ -1,6 +0,0 @@
-export {default as Navigation} from "./navbar";
-export {default as Footer} from "./Footer";
-export {default as About} from "./About";
-export {default as Contact} from "./Contact";
-export {default as Home} from "./Home";
-export {default as Services} from "./Services";
